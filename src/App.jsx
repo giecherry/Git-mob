b00ca41375c29ff3dd8771c86ac5968ed42aaf17
Angie C <a.cajusol98@gmail.com>
2024-12-06 11:14:45 +0100

    added pages for each App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import Calculator from "./pages/Calculator"
+import TodoList from "./pages/TodoList"
+import UserList from "./pages/UserList"
 
 function App() {
 
@@ -9,6 +12,9 @@ function App() {
       <h1>Git mob</h1>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
+        <Route path="/calculator" element={<Calculator/>}/>
+        <Route path="/todolist" element={<TodoList/>}/>
+        <Route path="/userlist" element={<UserList/>}/>
       </Routes>
     </>
   )
